337e3c9730a4271605a5b0a661c6185dd708bcf1
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2020-01-08 16:10:53 +0100

    przygotowanie templatów i plików statycznych zakończone

--- charity/urls.py
+++ charity/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from donations import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPageView.as_view(), name='index'),
+    path('add_donation/', views.AddDonationView.as_view(), name='add-donation'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
+
 ]
