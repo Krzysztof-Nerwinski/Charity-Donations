ebf0314e2c90b05e6d2649b29209183b1ec49ff4
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2020-01-08 22:52:07 +0100

    modele gotowe, jeszcze bez migracji i nieprzetestowane

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Institution(models.Model):
+    types = (
+        (1, 'Fundacja'),
+        (2, 'Organizacja pozarządowa'),
+        (3, 'Zbiórka lokalna')
+    )
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True)
+    type = models.IntegerField(choices=types, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT)
+    address = models.CharField(max_length=128)
+    phone_number = models.IntegerField(max_length=9)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=5)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_DEFAULT)
