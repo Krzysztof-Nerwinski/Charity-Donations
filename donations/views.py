6c26f7166f3fcd64a0607969ad4f1992a963a839
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2020-01-09 14:29:01 +0100

    dynamiczne worki i wparte organizacje, index.html + views.py

--- donations/views.py
+++ donations/views.py
@@ -1,10 +1,16 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+
+from donations.models import Donation
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        bags_quantity = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
+        backed_institutions = Donation.objects.distinct('institution').count()
+        return render(request, 'index.html', {'quantity': bags_quantity,
+                                              'institutions': backed_institutions})
 
 
 class AddDonationView(View):
