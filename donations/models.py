f4360a1f4066f68d16b4fa2f0f7070e079acb638
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2020-01-09 18:52:16 +0100

    dynamiczne wyświetlanie informacji o instytucjach na index.html

--- donations/models.py
+++ donations/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 
 class Institution(models.Model):
     types = (
--- donations/views.py
+++ donations/views.py
@@ -2,15 +2,21 @@ from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 
-from donations.models import Donation
+from donations.models import Donation, Institution
 
 
 class LandingPageView(View):
     def get(self, request):
         bags_quantity = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
         backed_institutions = Donation.objects.distinct('institution').count()
+        foundations = Institution.objects.filter(type=1)
+        nongov_institutions = Institution.objects.filter(type=2)
+        local_pickups = Institution.objects.filter(type=3)
         return render(request, 'index.html', {'quantity': bags_quantity,
-                                              'institutions': backed_institutions})
+                                              'backed_institutions': backed_institutions,
+                                              'foundations': foundations,
+                                              'nongov_institutions': nongov_institutions,
+                                              'local_pickups': local_pickups})
 
 
 class AddDonationView(View):
