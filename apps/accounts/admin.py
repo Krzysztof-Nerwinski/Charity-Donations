40535ce777a6ea0dd581da665efd815dd55cc7dd
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2020-04-02 12:44:46 +0200

    bug repair, check total admins - selected admins for deletion. Also some code refactoring done

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -31,47 +31,52 @@ change_is_staff_status.short_description = _('Zmień status zespołu dla wybrany
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    min_admin_objects = 2
-    message_min_admin_objects = _('Nie można usunąć wybranego superusera. '
-                                  'Na stronie musi pozostać co najmniej {} superuser/(ów)').format(min_admin_objects)
+    min_admins_number = 2
+    message_min_admins_number = _('Nie można usunąć części zaznaczonych userów. '
+                                  'Na stronie musi pozostać co najmniej {} superuser/(ów)').format(min_admins_number)
+    # list view
     actions = [change_is_active_status, change_is_staff_status]
-
     list_display = ['username', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active']
-
+    # add view
     add_form = CustomRegistrationForm
     add_fieldsets = (
         (_('Informacje o użytkowniku'), {'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}),
         (_('Uprawnienia'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
-
+    # change view
     form = CustomAdminUserChangeForm
     fieldsets = (
         (_('Informacje osobiste'), {'fields': ('username', 'password', 'first_name', 'last_name', 'email')}),
         (_('Uprawnienia'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
 
+    # don't allow single admin object removal when less admins than min_admins_number
     def has_delete_permission(self, request, obj=None):
         adminset = self.model.objects.filter(is_superuser=True)
-        if obj and obj.is_superuser and (adminset.count() <= self.min_admin_objects):
-            self.message_user(request, self.message_min_admin_objects, messages.ERROR)
+        if obj and obj.is_superuser and (adminset.count() <= self.min_admins_number):
+            self.message_user(request, self.message_min_admins_number, messages.ERROR)
             return False
         return super(CustomUserAdmin, self).has_delete_permission(request, obj)
 
+    # when using delete selected action remove admins from selection or stop action
     def response_action(self, request, queryset):
         selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
         admins = self.model.objects.filter(is_superuser=True)
         admins_pks = admins.values_list('pk', flat=True)
-        common_pks = [pk_num for pk_num in admins_pks if str(pk_num) in selected]
+        admins_in_selected = [pk_num for pk_num in admins_pks if str(pk_num) in selected]
+        admins_left = admins.count() - len(admins_in_selected)
         data = request.POST.copy()
-        if (admins.count() <= self.min_admin_objects) and common_pks and (data['action'] == 'delete_selected'):
-            selected = [int(pk_num) for pk_num in selected if not int(pk_num) in common_pks]
-            if selected:
+        delete_action = data['action'] == 'delete_selected'
+        enough_admins_left = admins_left >= self.min_admins_number
+        if delete_action and admins_in_selected and not enough_admins_left:
+            for pk_num in admins_in_selected:
+                selected.remove(str(pk_num))
+            if len(selected) > 0:
                 func = self.get_actions(request)[data['action']][0]
                 queryset = self.model.objects.filter(pk__in=selected)
-                self.message_user(request, self.message_min_admin_objects, messages.ERROR)
+                self.message_user(request, self.message_min_admins_number, messages.ERROR)
                 return func(self, request, queryset)
             else:
-                self.message_user(request, self.message_min_admin_objects, messages.ERROR)
-                pass
+                self.message_user(request, self.message_min_admins_number, messages.ERROR)
         else:
             return super(CustomUserAdmin, self).response_action(request, queryset)
